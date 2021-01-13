8db75758f0c63fb28478d640598dfe7589d8c63c
ld8 <don_lee@me.com>
2021-01-13 20:56:16 +0800

    init with options changed to sqlite and del mysql from package

--- config/database.js
+++ config/database.js
@@ -1,36 +1,15 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'mysql',
-        // 服务地址：前端也部署在同一个服务器上，所以本地host即可
-        host: env('DATABASE_HOST', 'localhost'),
-        // 服务端口号
-        port: env.int('DATABASE_PORT', 3306),
-        // 数据库名称 ‘strapi’
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
-      options: {},
+      options: {
+        useNullAsDefault: true,
+      },
     },
   },
 });
-
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'sqlite',
-//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-//       },
-//       options: {
-//         useNullAsDefault: true,
-//       },
-//     },
-//   },
-// });
